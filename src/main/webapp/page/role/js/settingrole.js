0556eb2bf2d4a32fd7dab222e4cf5fa279b0c1fc
166572731 <166572731@qq.com>
2019-05-22 22:26:47 +0800

    修改

--- src/main/webapp/page/role/js/settingrole.js
+++ src/main/webapp/page/role/js/settingrole.js
@@ -2,7 +2,7 @@ var pk_name = sessionStorage.getItem('pk_name');
 var pk_department = sessionStorage.getItem('pk_department');
 layui.use(['layer', 'form'], function () {
     var layer = layui.layer, form = layui.form;
-    top.window.parent.addtitle(dename,roname);
+
     $.post("../../loadMenus.do", function (data) {
         var menushtml = "";
         var onlymenus = '<div class="col-sm-6">\n' +
