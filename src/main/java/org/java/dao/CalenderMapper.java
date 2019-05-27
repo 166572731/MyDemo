0440a8d0777e97478fb71433e2784ce76148cae2
2842268753 <2842267853@qq.com>
2019-05-27 19:56:48 +0800

    2

---
+++ src/main/java/org/java/dao/CalenderMapper.java
@@ -0,0 +1,14 @@
+package org.java.dao;
+
+
+
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CalenderMapper {
+
+    public List<Map> load(@Param("departmentId") String departmentId,@Param("userId") Integer userId);
+}
